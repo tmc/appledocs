027d06224c1f1d1d2d34c9a9cef420ba617bc392
Travis Cline <travis.cline@gmail.com>
2025-10-17 02:20:13 -0700

    feat: add CoreFoundation framework bindings
    Generated CoreFoundation bindings with CF* types and functions.
    Package builds successfully.
    
    Part of: appledocs-22

---
+++ generated/corefoundation/gen.go
@@ -0,0 +1,8 @@
+// Code generated from Apple documentation for CoreFoundation. DO NOT EDIT.
+
+package corefoundation
+
+//go:generate rm -f *.gen.go
+//go:generate generate-framework-bindings -framework CoreFoundation -output .
+
+
---
+++ generated/corefoundation/types.gen.go
@@ -0,0 +1,207 @@
+// Code generated from Apple documentation for CoreFoundation. DO NOT EDIT.
+
+package corefoundation
+
+import "unsafe"
+
+// CFAllocatorRef is a CoreGraphics opaque type.
+type CFAllocatorRef unsafe.Pointer
+
+// CFArrayRef is a CoreGraphics opaque type.
+type CFArrayRef unsafe.Pointer
+
+// CFAttributedStringRef is a CoreGraphics opaque type.
+type CFAttributedStringRef unsafe.Pointer
+
+// CFBagRef is a CoreGraphics opaque type.
+type CFBagRef unsafe.Pointer
+
+// CFBinaryHeapRef is a CoreGraphics opaque type.
+type CFBinaryHeapRef unsafe.Pointer
+
+// CFBitVectorRef is a CoreGraphics opaque type.
+type CFBitVectorRef unsafe.Pointer
+
+// CFBooleanRef is a CoreGraphics opaque type.
+type CFBooleanRef unsafe.Pointer
+
+// CFBundleRef is a CoreGraphics opaque type.
+type CFBundleRef unsafe.Pointer
+
+// CFCalendarRef is a CoreGraphics opaque type.
+type CFCalendarRef unsafe.Pointer
+
+// CFCharacterSetRef is a CoreGraphics opaque type.
+type CFCharacterSetRef unsafe.Pointer
+
+// CFDataRef is a CoreGraphics opaque type.
+type CFDataRef unsafe.Pointer
+
+// CFDateFormatterRef is a CoreGraphics opaque type.
+type CFDateFormatterRef unsafe.Pointer
+
+// CFDateRef is a CoreGraphics opaque type.
+type CFDateRef unsafe.Pointer
+
+// CFDictionaryRef is a CoreGraphics opaque type.
+type CFDictionaryRef unsafe.Pointer
+
+// CFErrorRef is a CoreGraphics opaque type.
+type CFErrorRef unsafe.Pointer
+
+// CFFileDescriptorRef is a CoreGraphics opaque type.
+type CFFileDescriptorRef unsafe.Pointer
+
+// CFFileSecurityRef is a CoreGraphics opaque type.
+type CFFileSecurityRef unsafe.Pointer
+
+// CFLocaleRef is a CoreGraphics opaque type.
+type CFLocaleRef unsafe.Pointer
+
+// CFMachPortRef is a CoreGraphics opaque type.
+type CFMachPortRef unsafe.Pointer
+
+// CFMessagePortRef is a CoreGraphics opaque type.
+type CFMessagePortRef unsafe.Pointer
+
+// CFMutableArrayRef is a CoreGraphics opaque type.
+type CFMutableArrayRef unsafe.Pointer
+
+// CFMutableAttributedStringRef is a CoreGraphics opaque type.
+type CFMutableAttributedStringRef unsafe.Pointer
+
+// CFMutableBagRef is a CoreGraphics opaque type.
+type CFMutableBagRef unsafe.Pointer
+
+// CFMutableBitVectorRef is a CoreGraphics opaque type.
+type CFMutableBitVectorRef unsafe.Pointer
+
+// CFMutableCharacterSetRef is a CoreGraphics opaque type.
+type CFMutableCharacterSetRef unsafe.Pointer
+
+// CFMutableDataRef is a CoreGraphics opaque type.
+type CFMutableDataRef unsafe.Pointer
+
+// CFMutableDictionaryRef is a CoreGraphics opaque type.
+type CFMutableDictionaryRef unsafe.Pointer
+
+// CFMutableSetRef is a CoreGraphics opaque type.
+type CFMutableSetRef unsafe.Pointer
+
+// CFMutableStringRef is a CoreGraphics opaque type.
+type CFMutableStringRef unsafe.Pointer
+
+// CFNotificationCenterRef is a CoreGraphics opaque type.
+type CFNotificationCenterRef unsafe.Pointer
+
+// CFNumberFormatterRef is a CoreGraphics opaque type.
+type CFNumberFormatterRef unsafe.Pointer
+
+// CFNumberRef is a CoreGraphics opaque type.
+type CFNumberRef unsafe.Pointer
+
+// CFPlugInInstanceRef is a CoreGraphics opaque type.
+type CFPlugInInstanceRef unsafe.Pointer
+
+// CFPlugInRef is a CoreGraphics opaque type.
+type CFPlugInRef unsafe.Pointer
+
+// CFPropertyListRef is a CoreGraphics opaque type.
+type CFPropertyListRef unsafe.Pointer
+
+// CFReadStreamRef is a CoreGraphics opaque type.
+type CFReadStreamRef unsafe.Pointer
+
+// CFRunLoopObserverRef is a CoreGraphics opaque type.
+type CFRunLoopObserverRef unsafe.Pointer
+
+// CFRunLoopRef is a CoreGraphics opaque type.
+type CFRunLoopRef unsafe.Pointer
+
+// CFRunLoopSourceRef is a CoreGraphics opaque type.
+type CFRunLoopSourceRef unsafe.Pointer
+
+// CFRunLoopTimerRef is a CoreGraphics opaque type.
+type CFRunLoopTimerRef unsafe.Pointer
+
+// CFSetRef is a CoreGraphics opaque type.
+type CFSetRef unsafe.Pointer
+
+// CFSocketRef is a CoreGraphics opaque type.
+type CFSocketRef unsafe.Pointer
+
+// CFStringRef is a CoreGraphics opaque type.
+type CFStringRef unsafe.Pointer
+
+// CFStringTokenizerRef is a CoreGraphics opaque type.
+type CFStringTokenizerRef unsafe.Pointer
+
+// CFTimeZoneRef is a CoreGraphics opaque type.
+type CFTimeZoneRef unsafe.Pointer
+
+// CFTreeRef is a CoreGraphics opaque type.
+type CFTreeRef unsafe.Pointer
+
+// CFTypeRef is a CoreGraphics opaque type.
+type CFTypeRef unsafe.Pointer
+
+// CFURLEnumeratorRef is a CoreGraphics opaque type.
+type CFURLEnumeratorRef unsafe.Pointer
+
+// CFURLRef is a CoreGraphics opaque type.
+type CFURLRef unsafe.Pointer
+
+// CFUUIDRef is a CoreGraphics opaque type.
+type CFUUIDRef unsafe.Pointer
+
+// CFUserNotificationRef is a CoreGraphics opaque type.
+type CFUserNotificationRef unsafe.Pointer
+
+// CFWriteStreamRef is a CoreGraphics opaque type.
+type CFWriteStreamRef unsafe.Pointer
+
+// CFXMLNodeRef is a CoreGraphics opaque type.
+type CFXMLNodeRef unsafe.Pointer
+
+// CFXMLParserRef is a CoreGraphics opaque type.
+type CFXMLParserRef unsafe.Pointer
+
+// CFXMLTreeRef is a CoreGraphics opaque type.
+type CFXMLTreeRef unsafe.Pointer
+
+
+// Common CoreGraphics struct types
+type CGFloat = float64
+
+type CGPoint struct {
+	X CGFloat
+	Y CGFloat
+}
+
+type CGSize struct {
+	Width  CGFloat
+	Height CGFloat
+}
+
+type CGRect struct {
+	Origin CGPoint
+	Size   CGSize
+}
+
+type CGAffineTransform struct {
+	A  CGFloat
+	B  CGFloat
+	C  CGFloat
+	D  CGFloat
+	Tx CGFloat
+	Ty CGFloat
+}
+
+// Common type aliases
+type Range = CGPoint  // NSRange
+type Size = CGSize    // NSSize
+type Point = CGPoint  // NSPoint
+type Rect = CGRect    // NSRect
+type TimeInterval = float64  // NSTimeInterval
+
+
