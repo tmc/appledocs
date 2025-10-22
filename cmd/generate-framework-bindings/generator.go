5341130e486458fa8eb4c729d4abc1c97c87ec02
Travis Cline <travis.cline@gmail.com>
2025-10-22 14:56:33 -0700

    refactor(generator): Create generator_funcs.go for template helper methods
    Extract GeneratorFuncs type and formatMethodParams method from generator.go
    into new generator_funcs.go file. This establishes the foundation for Phase 3
    refactoring where template functions will be converted to GeneratorFuncs methods.
    
    - New file: generator_funcs.go with GeneratorFuncs struct and formatMethodParams method
    - Updated: generator.go - removed GeneratorFuncs definition, added reference comment
    - formatMethodParams uses O(1) indexes from Phase 1 (classIndex, enumIndex, typedefIndex)
    
    This is part of the GeneratorFuncs refactoring (Phase 3) being developed in
    parallel sessions. File organization follows the design in:
    /tmp/generator-funcs-organization.md

--- cmd/generate-framework-bindings/generator.go
+++ cmd/generate-framework-bindings/generator.go
@@ -163,41 +163,6 @@ func (g *Generator) IsTypedefType(typeName string) bool {
 // GeneratorFuncs wraps a Generator and provides template functions with access to
 // the Generator's data-driven type checking (enum/typedef/class indices).
 // This allows templates to use accurate type resolution instead of heuristics.
-type GeneratorFuncs struct {
-	*Generator
-}
-
-// formatMethodParams formats method parameters for Go function signatures using data-driven type checking.
-func (gf GeneratorFuncs) formatMethodParams(method *occ2go.ParsedMethod) string {
-	if len(method.Parameters) == 0 {
-		return ""
-	}
-
-	parts := make([]string, len(method.Parameters))
-	for i, p := range method.Parameters {
-		paramName := p.Name
-		if paramName == "" {
-			paramName = fmt.Sprintf("p%d", i)
-		}
-		if isGoKeyword(paramName) {
-			paramName += "_"
-		}
-
-		goType := mapObjCTypeToGo(p.Type, gf.Framework)
-
-		// Convert objc.ID to objectivec.IObject for better type safety
-		if goType == "objc.ID" {
-			goType = "objectivec.IObject"
-		} else {
-			// Use data-driven type checking instead of heuristics
-			goType = gf.TypeToInterfaceType(goType)
-		}
-
-		parts[i] = fmt.Sprintf("%s %s", paramName, goType)
-	}
-	return strings.Join(parts, ", ")
-}
-
 func (g *Generator) TypeToInterfaceType(goType string) string {
 	// Handle qualified types (e.g., "foundation.Coder" -> "foundation.ICoder")
 	if strings.Contains(goType, ".") {
---
+++ cmd/generate-framework-bindings/generator_funcs.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/tmc/appledocs/occ2go"
+)
+
+// GeneratorFuncs wraps Generator to provide template helper methods.
+// All methods have direct access to Generator state via embedding.
+//
+// This struct serves as the template function API, replacing scattered
+// standalone functions with methods that have proper access to Generator
+// data structures and indexes.
+type GeneratorFuncs struct {
+	*Generator
+}
+
+// Method Formatting
+// -----------------
+
+// formatMethodParams formats method parameters for Go function signatures using data-driven type checking.
+// This method uses the O(1) indexes from Phase 1 refactoring for efficient type lookups.
+func (gf GeneratorFuncs) formatMethodParams(method *occ2go.ParsedMethod) string {
+	if len(method.Parameters) == 0 {
+		return ""
+	}
+
+	parts := make([]string, len(method.Parameters))
+	for i, p := range method.Parameters {
+		paramName := p.Name
+		if paramName == "" {
+			paramName = fmt.Sprintf("p%d", i)
+		}
+		if isGoKeyword(paramName) {
+			paramName += "_"
+		}
+
+		goType := mapObjCTypeToGo(p.Type, gf.Framework)
+
+		// Convert objc.ID to objectivec.IObject for better type safety
+		if goType == "objc.ID" {
+			goType = "objectivec.IObject"
+		} else {
+			// Use data-driven type checking instead of heuristics
+			// This calls Generator.TypeToInterfaceType which uses classIndex, enumIndex, typedefIndex
+			goType = gf.TypeToInterfaceType(goType)
+		}
+
+		parts[i] = fmt.Sprintf("%s %s", paramName, goType)
+	}
+	return strings.Join(parts, ", ")
+}
+
+// Type Resolution
+// ---------------
+// TODO: Add more GeneratorFuncs methods here as they're converted from standalone functions
+
+// Name Conversion
+// ---------------
+// TODO: Add name conversion methods
+
+// Constructor Generation
+// ----------------------
+// TODO: Add constructor methods
+
+// Import Resolution
+// -----------------
+// TODO: Add import resolution methods
